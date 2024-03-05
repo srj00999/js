434e604f13e7706f9ecb343efc83d61ad282416e
Suraj Yadav <srj00999@gmail.com>
2024-03-05 05:21:47 +0000

    data types

---
+++ Basics/01_variables.js
@@ -0,0 +1,15 @@
+const rollNumber = 56;
+let myName =  "suraj";
+var myCity =  "Azamgarh";
+state = "uttarPradesh";
+let school;
+
+myName =  "rahul";
+myCity =  "lucknow";
+state = "MP";
+
+/* Prefer not to use var because of issue in block and functional scope */
+
+console.table([ myName, myCity, state, school]);
+
+
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node js not , browser
+
+let name  = "Suraj"
+let age =  18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value example getTemperature from server 
+//undefined => 
+//symbol => unique
+
+
+//object
+console.log(typeof(undefined)); //undefined
+console.log(typeof(null)); //object
