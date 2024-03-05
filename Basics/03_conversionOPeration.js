53bcf4c2ee524897ed2031261a6840dbeb3839fd
Suraj Yadav <srj00999@gmail.com>
2024-03-05 05:46:24 +0000

    conversion

---
+++ Basics/03_conversionOPeration.js
@@ -0,0 +1,25 @@
+let score  = true;
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueNumber = Number(score);
+console.log(valueNumber);
+console.log(typeof(valueNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true  =  1 ; false  => 0
+
+let isLoggedIN = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIN);
+console.log(booleanIsLoggedIn);
+
+// 1 => ture 
+// "" => false
+// "suraj" => true
+
+let someNumber = 33;
+let stringNumber  =  String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
